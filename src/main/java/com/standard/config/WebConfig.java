681889d668af0a5dca1c4936a251d8b85d1ea116
renz <renan.zazula@innofis.com>
2019-01-08 18:31:40 +0100

    Add web config

--- src/main/java/com/standard/config/WebConfig.java
+++ src/main/java/com/standard/config/WebConfig.java
@@ -1,5 +1,8 @@
 package com.standard.config;
 
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,5 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods(RequestMethod.GET.name(),
                 RequestMethod.PUT.name(), RequestMethod.POST.name(), RequestMethod.DELETE.name());
+    }
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
     }
 }
